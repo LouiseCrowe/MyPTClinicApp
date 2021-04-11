34154240d515c25844ce247ca92ef83803934cb3
LouiseCrowe <louise.crowe7@gmail.com>
2021-04-11 15:12:43 +0100

    Removing API Key from binary file

--- MyPTClinicApp/Server/Models/SendEmailRepository.cs
+++ MyPTClinicApp/Server/Models/SendEmailRepository.cs
@@ -28,36 +28,8 @@ namespace MyPTClinicApp.Server.Models
             }
             return true;
 
-<<<<<<< HEAD
-=======
 
 
-
-            //var apiKey = "INSERT API KEY HERE";
-            //var client = new SendGridClient(apiKey);
-            //var from = new EmailAddress("dylan@dylancroweclinic.ie", "Dylan");
-            //var to = new EmailAddress("x00159407@mytudublin.ie", "TUD");
-            //var to = new EmailAddress("louise_emily_murphy@hotmail.com", "Louise Hotmail");
-            //var to = new EmailAddress("louise.crowe7@gmail.com", "Louise Gmail");
-            //var subject = "Sending from Console App using SendGrid";
-            //var plainTextContent = "I'm very happy if this works";
-            //var htmlContent = "<strong>Sending from Console App</strong>";
-            //var msg = MailHelper.CreateSingleEmail(
-            //    from,
-            //    to,
-            //    subject,
-            //    plainTextContent, htmlContent
-            //    );
-
-            //Response response = await sendGridClient.SendEmailAsync(msg);
-            //if (Convert.ToInt32(response.StatusCode) >= 400)
-            //{
-            //    return false;
-            //}
-            //return true;
-
-
->>>>>>> b4281003485b411f63ef2cc2727846f973d28726
         }
     }
 }
