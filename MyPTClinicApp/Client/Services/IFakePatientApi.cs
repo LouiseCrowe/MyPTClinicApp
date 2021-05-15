4e725bb035b6f1c4dd208730e419200734f35c22
LouiseCrowe <louise.crowe7@gmail.com>
2021-05-15 09:34:35 +0100

    Adding finalised bUnit tests

---
+++ MyPTClinicApp/Client/Services/IFakePatientApi.cs
@@ -0,0 +1,14 @@
+﻿using MyPTClinicApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bUnitTestsWithXUnit
+{
+    public interface IFakePatientApi
+    {
+        Task<List<Patient>> GetPatientsAsync();
+    }
+}
